92d4733f8613f5e067b22ffad3d944e61c33ffcb
Mardoqueo Zotelo <mardoflorentin18@gmail.com>
2024-04-30 16:14:33 -0400

    ajustes especies

--- apps/especies/urls.py
+++ apps/especies/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('especies/', views.especies, name='especies'),
     path('especies/crear/', views.crear_especie, name='crear_especie'),
     path('especies/editar/<int:especie_id>/', views.editar_especie, name='editar_especie'),
-    path('especies/eliminar/<int:especie_id>/', views.eliminar_especie, name='eliminar_especie')
+    path('especies/eliminar/<int:especie_id>/', views.eliminar_especie, name='eliminar_especie'),
+    path('api/especies', views.api_especies, name='api_especies')
 ]
