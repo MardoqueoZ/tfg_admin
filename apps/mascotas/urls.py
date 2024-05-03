de9cda3324aba0e2389b810054c01f2abeb08aa4
Mardoqueo Zotelo <mardoflorentin18@gmail.com>
2024-05-02 23:07:54 -0400

    config api mascota

--- apps/mascotas/urls.py
+++ apps/mascotas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('duenhos/buscar', views.buscar_duenho, name='buscar_duenho'),
     path('mascotas/<int:cedula>', views.ver_mascotas, name='ver_mascotas'),
-    path('mascotas/registrar', views.registrar_mascota, name='registrar_mascota')
-]
+    path('mascotas/registrar', views.registrar_mascota)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
